7a269c9f6ad3b41bbc8a5a189aaa3ba8380bd105
Abhishek Mathur <abhishekmathur200624@gmail.com>
2025-12-30 16:43:08 +0530

    Refactor user and blog endpoints; update routes and add Alembic configuration(not completed)

--- routers/Blogs.py
+++ routers/Blogs.py
@@ -1,16 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException, Response
 from db import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
-from models import User
+from models import Blog
 from sqlalchemy import select, and_
 from typing import List, Optional
 # from schemas import UserBase, UserRead
 
 
-app = APIRouter(prefix="/user", tags=["users"])
+app = APIRouter(prefix="/blogs", tags=["Blogs"])
 
-@app.get("/Show-users")
+@app.get("/show-blogs")
 async def display(db: AsyncSession = Depends(get_db)):
-    stmt = select(User)
+    stmt = select(Blog)
     results = await db.execute(stmt)
     return results.scalar()
--- routers/__init__.py
+++ routers/__init__.py
@@ -1,5 +1,5 @@
 from routers.User import app as users
-# from routers.products import app as products_router
+from routers.Blogs import app as blogs
 # from routers.user import app as user_router
 # from routers.user_profile import app as users_profile_router
 
@@ -7,6 +7,6 @@ from fastapi import APIRouter
 
 app_router = APIRouter()
 app_router.include_router(users)
-# app_router.include_router(products_router)
+app_router.include_router(blogs)
 # app_router.include_router(user_router)
 # app_router.include_router(users_profile_router)
